4b7839df7991ecf136721edfeac176b9b6de6138
Max Gavryshkiv <maks20355@gmail.com>
2024-02-02 00:11:50 +0200

    Connected mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LocationModule } from './location/location.module';
+import { LocationsModule } from './locations/locations.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [LocationModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/locationsapi'),
+    LocationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
