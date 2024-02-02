f736640dd389f3e567d64f6e022d2a5e49fe7524
Max Gavryshkiv <maks20355@gmail.com>
2024-02-02 17:41:02 +0200

    Added docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LocationsModule } from './locations/locations.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/locationsapi'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(`${process.env.MONGODB_URI}${process.env.DB_NAME}`),
     LocationsModule,
   ],
   controllers: [AppController],
